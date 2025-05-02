d1973f6282d0c5f5ba6293a10b80b9f93120ae10
Beatriz Alcantara <beatrizalcantara813@gmail.com>
2025-05-02 15:14:48 -0300

    log cpf in cpf tests

--- Recognizer.Tests/CpfBaseRecognizerTests.cs
+++ Recognizer.Tests/CpfBaseRecognizerTests.cs
@@ -5,22 +5,17 @@ using Xunit.Abstractions;
 
 namespace Recognizer.Tests;
 
-public class CpfBaseRecognizerTests
+public class CpfBaseRecognizerTests(ITestOutputHelper outputHelper)
 {
     private readonly Faker _faker = new();
     private readonly CpfBaseRecognizer _baseRecognizer = new();
-    private readonly ITestOutputHelper _outputHelper;
 
-    public CpfBaseRecognizerTests(ITestOutputHelper outputHelper)
-    {
-        _outputHelper = outputHelper;
-    }
-    
     [Fact]
     public void GivenTextWithMoreThan14Digits_IsValid_ShouldReturnFalse()
     {
         var cpf = _faker.Random.Replace($"{GetRandomNumber()}.{GetRandomNumber()}.{GetRandomNumber()}-{GetRandomNumber()}");
-        _outputHelper.WriteLine(cpf);
+        outputHelper.WriteLine("CPF: {0}", cpf);
+        
         var result = _baseRecognizer.IsValid(cpf);
         
         result.Should().BeFalse();
@@ -32,7 +27,8 @@ public class CpfBaseRecognizerTests
     public void GivenTextWith14DigitsButIsNotInCorrectFormat_IsValid_ShouldReturnFalse()
     {
         var cpf = _faker.Random.Replace("##############");
-
+        outputHelper.WriteLine("CPF: {0}", cpf);
+        
         var result = _baseRecognizer.IsValid(cpf);
         
         result.Should().BeFalse();
@@ -46,6 +42,7 @@ public class CpfBaseRecognizerTests
     public void GivenTextWithInvalidFormat_IsValid_ShouldReturnFalse(string format)
     {
         var cpf = _faker.Random.Replace(format);
+        outputHelper.WriteLine("CPF: {0}", cpf);
         
         var result = _baseRecognizer.IsValid(cpf);
         
@@ -58,6 +55,7 @@ public class CpfBaseRecognizerTests
     public void GivenTextWithValidFormatWithLetters_IsValid_ShouldReturnFalse(string format)
     {
         var cpf = _faker.Random.Replace(format);
+        outputHelper.WriteLine("CPF: {0}", cpf);
         
         var result = _baseRecognizer.IsValid(cpf);
         
@@ -68,6 +66,7 @@ public class CpfBaseRecognizerTests
     public void GivenTextWithValidCpf_IsValid_ShouldReturnTrue()
     {
         var cpf = _faker.Random.Replace("###.###.###-##");
+        outputHelper.WriteLine("CPF: {0}", cpf);
 
         var result = _baseRecognizer.IsValid(cpf);
         
