eb66a52209cb0ec7e37fefc6aaad936e05cde991
Beatriz Alcantara <beatrizalcantara813@gmail.com>
2025-05-03 14:10:20 -0300

    Implement Phone Number recognizer

--- Recognizer.Core/PhoneNumberBaseRecognizer.cs
+++ Recognizer.Core/PhoneNumberBaseRecognizer.cs
@@ -2,4 +2,5 @@
 
 public class PhoneNumberBaseRecognizer : BaseRecognizer
 {
+    protected override string Pattern => @"(\(\d\d\)\s9\d{4}-\d{4})|(\(\d\d\)\s9\d{8})|(\d\d\s9\d{8})";
 }
--- Recognizer.Tests/PhoneNumberBaseRecognizerTests.cs
+++ Recognizer.Tests/PhoneNumberBaseRecognizerTests.cs
@@ -1,6 +1,54 @@
-﻿namespace Recognizer.Tests;
+﻿using Bogus;
+using FluentAssertions;
+using Recognizer.Core;
+using Xunit.Abstractions;
 
-public class PhoneNumberBaseRecognizerTests
+namespace Recognizer.Tests;
+
+public class PhoneNumberBaseRecognizerTests(ITestOutputHelper outputHelper)
 {
+    private readonly PhoneNumberBaseRecognizer _recognizer = new PhoneNumberBaseRecognizer();
+    private readonly Faker _faker = new Faker();
+
+    [Theory]
+    [InlineData("## 9####-####")]
+    [InlineData("(##)9####-####")]
+    [InlineData("(##)9########")]
+    [InlineData("##9####-####")]
+    [InlineData("##9########")]
+    public void GivenPhoneNumberInInvalidFormat_IsValid_ShouldReturnFalse(string format)
+    {
+        var phoneNumber = _faker.Phone.PhoneNumber(format);
+        outputHelper.WriteLine("PhoneNumber: {0}", phoneNumber);
+        
+        var result = _recognizer.IsValid(phoneNumber);
+
+        result.Should().BeFalse();
+    }
     
+    [Fact]
+    public void GivenPhoneNumberStatingWithNumberDifferentOf9_IsValid_ShouldReturnFalse()
+    {
+        var firstNumber = _faker.Random.Int(1, 8);
+        var phoneNumber = _faker.Phone.PhoneNumber($"(##) {firstNumber}####-####");
+        outputHelper.WriteLine("PhoneNumber: {0}", phoneNumber);
+        
+        var result = _recognizer.IsValid(phoneNumber);
+
+        result.Should().BeFalse();
+    }
+    
+    [Theory]
+    [InlineData("## 9########")]
+    [InlineData("(##) 9####-####")]
+    [InlineData("(##) 9########")]
+    public void GivenPhoneNumberWithValidFormat_IsValid_ShouldReturnTrue(string format)
+    {
+        var phoneNumber = _faker.Phone.PhoneNumber(format);
+        outputHelper.WriteLine("PhoneNumber: {0}", phoneNumber);
+        
+        var result = _recognizer.IsValid(phoneNumber);
+
+        result.Should().BeTrue();
+    }
 }
